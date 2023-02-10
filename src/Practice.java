2191e7660066b36642797b6b99c9c47232243644
Stepan <stepan-morozov-1980@mail.ru>
2023-02-10 23:08:14 +0400

    ...

---
+++ src/Practice.java
@@ -0,0 +1,16 @@
+public class Practice {
+    public static void main(String[] args) {
+        practice1(); // Напишите программу, которая проверяет, является ли строка палиндромом
+    }
+
+    public static void practice1() {
+        String word = "АнНА";
+        String word1 = new String();
+        for (int i = 0; i < word.length(); i++) {
+            word1 = word.charAt(i) + word1;
+        }
+        System.out.println(word.equalsIgnoreCase(word1));
+        word1 = new StringBuilder(word).reverse().toString();
+        System.out.println(word.equalsIgnoreCase(word1));
+    }
+}
